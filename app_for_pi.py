9b3ace4395d63f292af3e1415dadbf15be063f92
justinwhitfieldd <justinwhitfieldd@gmail.com>
2023-11-14 21:51:32 -0600

    cleaned up repo

--- app_for_pi.py
+++ app_for_pi.py
@@ -3,49 +3,59 @@ import RPi.GPIO as GPIO
 import time
 app = Flask(__name__)
 
-relay_pin1 = 8  # IN1 on the relay board, physical pin 14
-relay_pin2 = 10  # IN2 on the relay board, physical pin 15
-relay_pin3 = 12   # IN3 on the relay board, physical pin 18
-relay_pin4 = 16  # IN4 on the relay board, physical pin 23
+rev_motor = 8  # IN1 on the relay board, physical pin 14
+fire = 10  # IN2 on the relay board, physical pin 15
 # Setup GPIO
-GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-GPIO.setup(relay_pin1, GPIO.OUT)  # Set relay pins to be an output
-GPIO.setup(relay_pin2, GPIO.OUT)
-GPIO.setup(relay_pin3, GPIO.OUT)
-GPIO.setup(relay_pin4, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(rev_motor, GPIO.OUT) 
+GPIO.setup(fire, GPIO.OUT)
 
 # Initial state for relays
-GPIO.output(relay_pin1, GPIO.LOW)
-GPIO.output(relay_pin2, GPIO.LOW)
-GPIO.output(relay_pin3, GPIO.LOW)
-GPIO.output(relay_pin4, GPIO.LOW)
+GPIO.output(rev_motor, GPIO.LOW)
+GPIO.output(fire, GPIO.LOW)
+
 
 def toggle_relay(pin, state):
     """Toggle the relay to the desired state (True for ON, False for OFF)."""
     GPIO.output(pin, GPIO.HIGH if state else GPIO.LOW)
 
-toggle_relay(relay_pin1, True)
-toggle_relay(relay_pin4, True)
-toggle_relay(relay_pin3, False)
-toggle_relay(relay_pin2, True)
+toggle_relay(rev_motor, True)
+toggle_relay(fire, True)
 
-@app.route('/receive_command', methods=['POST'])
-def receive_command():
+@app.route('/rev_motor', methods=['POST'])
+def rev_motor():
+    toggle_relay(rev_motor, False)
+    # Return a success response
+    return jsonify({'status': 'success', 'message': 'shot fired'}), 200
+
+@app.route('/stop_rev_motor', methods=['POST'])
+def stop_rev_motor():
+    toggle_relay(rev_motor, True)
+    # Return a success response
+    return jsonify({'status': 'success', 'message': 'shot fired'}), 200
+
+@app.route('/shoot', methods=['POST'])
+def shoot():
+
+    toggle_relay(fire, False)
+    time.sleep(0.3)
+    toggle_relay(fire, True)
+    # Return a success response
+    return jsonify({'status': 'success', 'message': 'shot fired'}), 200
+
+@app.route('/rev_and_shoot', methods=['POST'])
+def rev_and_shoot():
     # Get JSON data from the request
     data = request.get_json()
 
-    toggle_relay(relay_pin1, False)
-    toggle_relay(relay_pin4, False)
-    toggle_relay(relay_pin3, True)
-    toggle_relay(relay_pin2, False)
-    time.sleep(3)
-    toggle_relay(relay_pin1, True)
-    toggle_relay(relay_pin4, True)
-    toggle_relay(relay_pin3, False)
-    toggle_relay(relay_pin2, True)
-
+    toggle_relay(rev_motor, False)
+    time.sleep(1)
+    toggle_relay(fire, False)
+    time.sleep(0.3)
+    toggle_relay(fire, True)
+    toggle_relay(rev_motor, True)
     # Return a success response
     return jsonify({'status': 'success', 'message': 'shot fired'}), 200
 
 if __name__ == '__main__':
-    app.run(port=3002)  # Run on a different port if 3001 is used by the Node.js server
+    app.run(port=3002)  # Run on a different port since 3001 is used by the Node.js server
